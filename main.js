d631710b6e3a5bb1d48a8ab8d14a4291a273f3d7
Lauryn Thayne <lalder57@gmail.com>
2024-08-12 15:10:13 -0600

    everything working

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const initSharkwords = () => {
   setupWord(document.querySelector("#word-container"), word);
   console.log(isLetterInWord(word[0]), '(should be true)');
   console.log(isLetterInWord('1'), '(should be false)');
-  revealLetterInWord(word[0]);
+  // revealLetterInWord(word[0]);
 
   const handleGuess = (guessEvent, letter) => {
     console.log(letter);
@@ -41,8 +41,31 @@ const initSharkwords = () => {
       numWrong += 1;
       setSharkImage(document.querySelector("#shark-img"), numWrong);
     }
-    console.log(numWrong);
-  };
+
+    // Check to see if the game ends
+    let isWordComplete = true;
+    for (const el of document.querySelectorAll(".letter-box")) {
+      console.log(el.innerText);
+
+      if (el.innerText === "") {
+        isWordComplete = false;
+        break;
+      } 
+    }
+
+    if (numWrong >= 5) {
+      document.querySelector("#game-status").innerText = "You lose!";
+      document.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('disabled', true);
+      });
+    } else if (isWordComplete) {
+      document.querySelector("#game-status").innerText = "You win!";
+      document.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('disabled', true);
+      });
+    }
+  }
+  
 
   setupGuesses(document.querySelector("#letter-buttons"), handleGuess);
   setSharkImage(document.querySelector("#shark-img"), numWrong);
